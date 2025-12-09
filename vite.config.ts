f565d9326dbc2d8a794799792590c650b50ab282
minibell7 <tjdudaud@gmail.com>
2025-12-09 16:14:06 +0900

    build: Increase chunk size warning limit to 1000kB

--- vite.config.ts
+++ vite.config.ts
@@ -49,6 +49,9 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       }
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
     }
   };
 });
